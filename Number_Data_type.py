10081d7489bee8c0422963a40d3d38a2074db66a
pushpjeet <pushpjeet@gmail.com>
2021-08-28 23:27:56 +0530

    Initial commit

---
+++ Number_Data_type.py
@@ -0,0 +1,20 @@
+# Write Your First Python Program
+print(type("Hello World!"))
+# Number type -->1) int    2) float   3) complex
+print(80)
+print(type(110.45))
+print(type(66))
+#binary 
+#Octal
+#Hexademical
+num1 =40
+num2 = 50
+sum =num1 + num2
+print("The sum of the two numbers is ", sum)
+print('The sum of {0} and {1}  is {2}'.format(num1,num2,sum))
+#Getting value from the user by using the input function
+num1=input('Enter the first Value: ')
+num2=input('Enter the second value: ')
+#Adding the numbers
+sum=num1+num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
---
+++ chapter1.py
@@ -0,0 +1,7 @@
+print("Hello this is my first python program")
+
+num1=20
+num2=90
+sum=float(num1)+ float(num2)
+print(sum)
+
